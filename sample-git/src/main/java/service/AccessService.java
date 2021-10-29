943f7136a4d18ed1b1b9ae955102257a46260457
Saranya1062 <saranya@synergyfolks.com>
2021-10-29 07:59:42 +0530

    modified in sample file

---
+++ sample-git/src/main/java/service/AccessService.java
@@ -0,0 +1,16 @@
+package service;
+
+public class AccessService {
+    public static void main(String[] args) {
+        System.out.println("service");
+        System.out.println("changed");
+        System.out.println("patch");
+        System.out.println("1");
+    }
+
+    public void Success() {
+        int a = 2;
+        int b = 5;
+        int c = 6;
+    }
+}
